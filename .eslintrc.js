59ea6bdfeded4a3db96d228a563d145a623d6579
txtyb <txtybbill@163.com>
2022-04-29 20:54:03 +0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      'vue/setup-compiler-macros': true
+    }, 
+    extends: [
+        'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-recommended'
+    ]
+}
