745501f9362da0a50e6b2d561d31de408b39b96e
txtyb <txtybbill@163.com>
2022-04-30 17:17:29 +0800

    Reinstall element-plus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
+import App from './App.vue'
 
 const app = createApp(App)
-installElementPlus(app)
+
+app.use(ElementPlus)
 app.use(VueChartkick)
 app.mount('#app')
