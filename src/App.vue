0055f71e5a713f01cfd2ae31bad09c04a0086a43
txtyb <txtybbill@163.com>
2022-05-03 16:53:16 +0800

    Fix the autoRefresh switch

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,84 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { Check, Close } from "@element-plus/icons-vue";
-const setAutoRefresh = ref(false);
 
-const dataNum = ref(50);
+// const dataNum = ref(50);
+
+// const rhdata = ref("");
+// const temdata = ref("");
+// const tableData = ref([]);
+
+// function refresh() {
+//   console.log(`dataNum = ${dataNum.value}`);
+//   var url =
+//     "https://temsite-serverless-txtyb.vercel.app/api/gettem?n=" + dataNum.value;
+//   fetch(url, {
+//     method: "get",
+//   })
+//     // .then(data => data.text())
+//     .then((data) => data.json())
+//     .then((data) => (temdata.value = data));
+//   // updateData(url, this.temdata);
+//   url =
+//     "https://temsite-serverless-txtyb.vercel.app/api/getrh?n=" + dataNum.value;
+//   fetch(url, {
+//     method: "get",
+//   })
+//     // .then(data => data.text())
+//     .then((data) => data.json())
+//     .then((data) => (rhdata.value = data));
+//   url = "https://temsite-serverless-txtyb.vercel.app/api/get?ts=0";
+//   fetch(url, {
+//     method: "get",
+//   })
+//     // .then(data => data.text())
+//     .then((data) => data.json())
+//     .then((data) => (tableData.value = data));
+//   console.log("refreshed");
+// }
+
+// const refreshSwitch = () => {
+//   var interval;
+//   const disableInterval = () => {
+//     clearInterval(interval);
+//   };
+//   if (setAutoRefresh.value) {
+//     interval = setInterval(() => {
+//       refresh();
+//     }, 5000);
+//     console.log(setAutoRefresh.value);
+//     console.log("enable auto refresh");
+//   } else {
+//     disableInterval
+//     console.log(setAutoRefresh.value);
+//     console.log("disable auto refresh");
+//   }
+// };
+
+// onMounted(refresh);
 
 defineExpose({
-  setAutoRefresh,
-  dataNum,
+  // setAutoRefresh,
+  // dataNum,
+  // temdata,
+  // rhdata,
+  // tableData,
 });
 </script>
 
 <script lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
+// // import HelloWorld from './components/HelloWorld.vue'
 // import { ref } from "vue"
+
+const dataNum = ref(50);
+
+const rhdata = ref("");
+const temdata = ref("");
+const tableData = ref([]);
+
+const setAutoRefresh = ref(false);
 
 export default {
   // el: "#app",
@@ -32,36 +95,37 @@ export default {
   // },
   data() {
     return {
+      // // temdata: "",
+      // rhdata: "",
       // temdata: "",
-      rhdata: "",
-      temdata: "",
-      tableData: [],
-      // setAutoRefresh: ref(false),
-      // dataNum: ref(50),
+      // tableData: [],
+      // // setAutoRefresh: ref(false),
+      // // dataNum: ref(50),
     };
   },
   computed: {
-    fetchData() {
-      return { "2021-01-01": 2, "2021-01-02": 3 };
-    },
+    // fetchData() {
+    //   return { "2021-01-01": 2, "2021-01-02": 3 };
+    // },
     // temdata: gettem()
   },
-  created() {},
+  created() { },
   mounted() {
+    console.log(`print setAutoRefresh = ${setAutoRefresh.value}`)
     this.refresh();
     this.test();
   },
   methods: {
-    // gettem() {
-    //   var data;
-    //   // var response = fetch("https://temsite-serverless-txtyb.vercel.app/api/gettem", {
-    //   //   method: "get",
-    //   // })
-    //   fetch("https://temsite-serverless-txtyb.vercel.app/api/gettem", {method: 'get'})
-    //   .then(res => res.json())
-    //   .then(json => data = json)
-    //   return data
-    // },
+    // // gettem() {
+    // //   var data;
+    // //   // var response = fetch("https://temsite-serverless-txtyb.vercel.app/api/gettem", {
+    // //   //   method: "get",
+    // //   // })
+    // //   fetch("https://temsite-serverless-txtyb.vercel.app/api/gettem", {method: 'get'})
+    // //   .then(res => res.json())
+    // //   .then(json => data = json)
+    // //   return data
+    // // },
     refresh() {
       // function updateData(url, dst) {
       //   fetch(url, {
@@ -74,39 +138,39 @@ export default {
       // }
       var url =
         "https://temsite-serverless-txtyb.vercel.app/api/gettem?n=" +
-        this.dataNum;
+        dataNum.value;
       fetch(url, {
         method: "get",
       })
         // .then(data => data.text())
         .then((data) => data.json())
-        .then((data) => (this.$data.temdata = data));
+        .then((data) => (temdata.value = data));
       // updateData(url, this.temdata);
       url =
         "https://temsite-serverless-txtyb.vercel.app/api/getrh?n=" +
-        this.dataNum;
+        dataNum.value;
       fetch(url, {
         method: "get",
       })
         // .then(data => data.text())
         .then((data) => data.json())
-        .then((data) => (this.$data.rhdata = data));
+        .then((data) => (rhdata.value = data));
       url = "https://temsite-serverless-txtyb.vercel.app/api/get?ts=0";
       fetch(url, {
         method: "get",
       })
         // .then(data => data.text())
         .then((data) => data.json())
-        .then((data) => (this.$data.tableData = data));
+        .then((data) => (tableData.value = data));
       console.log("refreshed");
     },
     test() {
       console.log("this is test");
     },
     // 切换自动刷新
-    refreshSwitch() {
-      console.log(this.setAutoRefresh);
-      if (!this.setAutoRefresh) {
+    refreshSwitch(this: any) {
+      console.log(setAutoRefresh.value);
+      if (!setAutoRefresh.value) {
         clearInterval(this.autoRefresh);
         console.log("Auto refresh disabled");
       } else {
@@ -130,71 +194,47 @@ export default {
           </el-col>
           <el-col :span="8">
             <div class="slider-block">
-              <el-slider
-                v-model="dataNum"
-                show-input
-                @click="refresh"
-                label="123"
-              />
+              <el-slider v-model="dataNum" show-input @click="refresh" label="123" />
             </div>
           </el-col>
-          <el-col :span="4">
-            <el-button type="primary" @click="refresh"
-              >刷新 {{ setAutoRefresh }}</el-button
-            >
+          <el-col :span="2">
+            <el-button type="primary" @click="refresh">刷新 {{ setAutoRefresh }}</el-button>
           </el-col>
-          <el-col :span="2" :pull="1">
-            <el-switch
-              class="refreshSwitch"
-              v-model="setAutoRefresh"
-              active-text="自动刷新"
-              inline-prompt
-              :active-icon="Check"
-              :inactive-icon="Close"
-              @change="refreshSwitch"
-            />
+          <el-col :span="4" :pull="1">
+            <span class="switch-text">自动刷新</span>
+            <el-switch class="refreshSwitch" v-model="setAutoRefresh" inline-prompt :active-icon="Check"
+              :inactive-icon="Close" @change="refreshSwitch" />
           </el-col>
         </el-row>
       </el-header>
       <el-main>
         <el-card class="box-card">
-          <el-space direction="horizontal" :fill="true" warp class="charts">
-            <div class="chart">
-              <line-chart
-                :data="temdata"
-                :min="null"
-                :max="null"
-                xtitle="时间"
-                ytitle="温度℃"
-              ></line-chart>
-            </div>
-            <el-divider />
-            <div class="chart">
-              <area-chart
-                :data="rhdata"
-                :min="null"
-                :max="null"
-                xtitle="时间"
-                ytitle="湿度%"
-              ></area-chart>
-            </div>
-            <!-- <area-chart
+          <!-- <el-space direction="horizontal" :fill="true" wrap class="charts"> -->
+          <div class="chart">
+            <line-chart :data="temdata" :min="null" :max="null" xtitle="时间" ytitle="温度℃"></line-chart>
+          </div>
+          <el-divider />
+          <div class="chart">
+            <area-chart :data="rhdata" :min="null" :max="null" xtitle="时间" ytitle="湿度%"></area-chart>
+          </div>
+          <!-- <area-chart
             :data="fetchData"
             :min="null"
             :max="null"
             xtitle="时间"
             ytitle="湿度%"
           ></area-chart> -->
-          </el-space>
+          <!-- </el-space> -->
         </el-card>
         <el-card class="tables">
-          <!-- <el-space direction="horizonal" :fill="true" warp> -->
-          <el-table :data="tableData" style="width: 100%" max-height="250">
-            <el-table-column fixed prop="time" label="时间" width="600" />
-            <el-table-column prop="tem" label="温度" width="120" />
-            <el-table-column fixed="right" prop="rh" label="湿度" width="120" />
-          </el-table>
-          <!-- </el-space> -->
+          <div>
+            <el-table :data="tableData" style="width: 100%" max-height="500" stripe fit > 
+              <el-table-column type="index" width="70" />
+              <el-table-column prop="time" label="时间" width="800" />
+              <el-table-column prop="tem" label="温度" />
+              <el-table-column prop="rh" label="湿度" />
+            </el-table>
+          </div>
         </el-card>
       </el-main>
       <el-footer>
@@ -219,22 +259,31 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .chart {
   margin: 15px;
 }
+
 .title {
   font-weight: bold;
   font-size: 25px;
 }
+
 .refreshSwitch {
-  /* margin-left: -80px; */
-  margin-top: 8px;
+  margin-top: 5px;
 }
+
 /* .charts {
   overflow: hidden;
 } */
 .slider-block {
   /* display: flex; */
   align-items: center;
+}
+
+.switch-text {
+  margin-left: -20%;
+  margin-right: 5%;
+  margin-top: 10px;
 }
 </style>
